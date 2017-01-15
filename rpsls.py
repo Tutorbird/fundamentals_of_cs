c27155a32e84e3151178afb0dfd9929cc7d9cbdb
Nestor J. Alvarez <grimreaper1169@gmail.com>
2017-01-15 14:11:05 -0800

    Write final code for RPSLS mini-project.

--- rpsls.py
+++ rpsls.py
@@ -1,5 +1,5 @@
 # Rock-paper-scissors-lizard-Spock template
-
+import random
 
 # The key idea of this program is to equate the strings
 # "rock", "paper", "scissors", "lizard", "Spock" to numbers
@@ -31,12 +31,27 @@ def number_to_name(number):
         3: 'lizard',
         4: 'scissors'
     }.get(number, None)
-    
+
+def print_screen(player_choice, computer_choice):
+    print "Player chooses %s", player_choice
+    print "Computer chooses %s", computer_choice
 
 def rpsls(player_choice): 
     # delete the following pass statement and fill in your code below
-    pass
-    
+
+    print '\n'
+    player_number = name_to_number(player_choice)
+    comp_number = random.randrange(0, 5)
+    computer_choice = number_to_name(comp_number)
+    difference = comp_number - player_number % 5
+
+    if difference > 2 :
+        print_screen(player_choice, computer_choice)
+        print "Computer wins!"
+    else:
+        print_screen(player_choice, computer_choice)
+        print "Player wins!"
+
     # print a blank line to separate consecutive games
 
     # print out the message for the player's choice

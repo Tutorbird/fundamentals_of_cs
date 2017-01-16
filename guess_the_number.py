22ca2543c6ab7adad649f7b12fe6479474f1c7d2
Nestor J. Alvarez <grimreaper1169@gmail.com>
2017-01-16 11:48:48 -0800

    Implement event handlers to change the ranges.

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,28 +1,32 @@
-# Nestor Alvarez, 20170116
+# template for "Guess the number" mini-project
+# input will come from buttons and an input field
+# all output for the game will be printed in the console
 
 import simplegui, random, math
 
-
+secret_number = 0
+secret_range = 100
 
 # helper function to start and restart the game
 def new_game():
     # initialize global variables used in your code here
-
-    # remove this when you add your code    
-    pass
-
+    global secret_number
+    secret_number = random.randrange(0,secret_range)
+    
+    print "Range", secret_range, "| Number", secret_number
 
 # define event handlers for control panel
 def range100():
-    # button that changes the range to [0,100) and starts a new game 
-    
-    # remove this when you add your code    
-    pass
+    global secret_range 
+    secret_range = 100
+    new_game()
+    print "Range changed to 100"
 
 def range1000():
-    # button that changes the range to [0,1000) and starts a new game     
-    
-    pass
+    global secret_range 
+    secret_range = 1000
+    new_game()
+    print "Range changed to 100"
     
 def input_guess(guess):
     # main game logic goes here 

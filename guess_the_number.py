e1bd3a2f35f67a7d8d6221815f469403d0e4a2fd
Nestor J. Alvarez <grimreaper1169@gmail.com>
2017-01-16 12:51:20 -0800

    Add UI messages to help the player. Remove debugging comment.

--- guess_the_number.py
+++ guess_the_number.py
@@ -7,6 +7,7 @@ import simplegui, random, math
 secret_number = 0
 secret_range = 100
 guesses = 0
+message = "Welcome!"
 
 # helper function to start and restart the game
 
@@ -19,6 +20,8 @@ def new_game():
     global secret_number, guesses
     secret_number = random.randrange(0,secret_range)
     guesses = int(math.ceil(math.log(secret_range - 1) / math.log(2)))
+    frame.set_draw_handler(draw)
+    print "The range is 0 to", secret_range
     return None 
 
 def decrement():
@@ -36,17 +39,25 @@ def validate(guess):
 
 def print_screen(flag, guess):
     # -1 is lower, 0 is equal, 1 is higher
+    global message
+    
     print "Guess was", guess
     print "Number of guesses left:", guesses
     
     if flag == -1:
         print "Lower!"
+        message = "Lower!"
+        
     elif flag == 0:
         print "Correct!"
+        message = "Good Job!"
     elif flag == 1:
         print "Higher!"
+        message = "Higher!"
     else:
+        message = "Error!"
         print "Something weird happen. Our top coding monkeys are on it."
+    frame.set_draw_handler(draw)
     return None
         
 # define event handlers for control panel
@@ -61,6 +72,9 @@ def range1000():
     secret_range = 1000
     new_game()
     return None
+
+def draw(canvas):
+    canvas.draw_text(message, [50,112], 48, "Red")
     
 def input_guess(guess):
     # main game logic goes here 
@@ -71,7 +85,7 @@ def input_guess(guess):
             print "\nYou are out of guesses"
             print "Starting new game... \n"
             new_game()
-        elif guess > secret_number:
+        elif guess < secret_number:
             print_screen(1, guess)
         elif guess == secret_number:
             print_screen(0, guess)
@@ -80,7 +94,7 @@ def input_guess(guess):
 
     
 # create frame
-frame = simplegui.create_frame("Guess the number!", 200, 300)
+frame = simplegui.create_frame("Guess the number!", 300, 200)
 
 
 

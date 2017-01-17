4a19e52fa3a667ede428013613c058c48b7e1fae
Nestor J. Alvarez <grimreaper1169@gmail.com>
2017-01-16 19:23:54 -0800

    Boilerplater for Stopwatch game.

---
+++ stopwatch_game.py
@@ -0,0 +1,29 @@
+# Nestor Alvarez, 20170116
+
+# define global variables
+
+
+# define helper function format that converts time
+# in tenths of seconds into formatted string A:BC.D
+def format(t):
+    pass
+    
+# define event handlers for buttons; "Start", "Stop", "Reset"
+
+
+# define event handler for timer with 0.1 sec interval
+
+
+# define draw handler
+
+    
+# create frame
+
+
+# register event handlers
+
+
+# start frame
+
+
+# Please remember to review the grading rubric

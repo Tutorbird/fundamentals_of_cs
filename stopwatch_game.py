a48bfadfb09e4804f4af4304e77ec47cd13a3758
Nestor J. Alvarez <grimreaper1169@gmail.com>
2017-01-16 19:40:40 -0800

    Make text variables dynamic.

--- stopwatch_game.py
+++ stopwatch_game.py
@@ -3,6 +3,9 @@ import time, simplegui
 
 # define global variables
 deciseconds = 0
+attempts = 0
+score = 0
+message = "0:00:0"
 
 # define helper function format that converts time
 # in tenths of seconds into formatted string A:BC.D
@@ -20,8 +23,9 @@ def time_handler():
     return None
 
 def draw_handler(canvas):
-    canvas.draw_text('X / Y', (200, 50), 20, 'Green')
-    canvas.draw_text('0:00:0', (95, 110), 40, 'Red')
+    scoreboard = "" +  str(score) + "/" + str(attempts) + ""
+    canvas.draw_text(scoreboard, (200, 50), 20, 'Green')
+    canvas.draw_text(message, (95, 110), 40, 'Red')
 
 # define draw handler
 

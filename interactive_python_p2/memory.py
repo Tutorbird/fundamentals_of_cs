e45120239b22099e9abb51497e3cdf4eaa73d29e
Nestor J. Alvarez <grimreaper1169@gmail.com>
2017-03-09 20:46:35 -0800

    Finished assignment. Not hard, but like always I like making my code as pristine as possible. I am happy with it. I took a look online after the fact and saw some pretty big lines and way crazier if-else branches.

--- interactive_python_p2/memory.py
+++ interactive_python_p2/memory.py
@@ -6,12 +6,10 @@ import simplegui, random
 
 CARD_WIDTH = 50
 CARD_LENGTH = 100
-count = 0
-guess = 0
 
 # helper function to initialize globals
 def new_game():
-    global deck, exposed, cleared, prev
+    global deck, exposed, cleared, prev, guess, count
     deck = range(0, 8)
     deck.extend(deck)
     random.shuffle(deck)
@@ -20,6 +18,7 @@ def new_game():
     prev = -1
     count = 0
     guess = 0
+    label.set_text("Turns = " + str(count))
     print deck
     pass  
 
@@ -28,23 +27,28 @@ def new_game():
 def mouseclick(pos):
     global prev, count, exposed, guess
     card_pos = pos[0] / 50
+    temp = card_pos
     if prev != card_pos:
-        if exposed[card_pos] == 0:
+        guess += 1
+        if deck[prev] == deck[card_pos] and guess == 2:
+            # Need this otherwise we are adding wrong values
+            if cleared[card_pos] != 1:
+                count += 1
+            # The above reason is the main reason why we need
+            # Two lists. One is meant to be semi-immutable for 
+            # Logic purposes
+            cleared[prev] = 1
+            cleared[card_pos] = 1
+            exposed = [0] * 16
+            prev = -1
+            guess = 0
+        exposed[card_pos] = 1
+        if guess == 3:
+            exposed = [0] * 16
             exposed[card_pos] = 1
-            if deck[prev] == deck[card_pos]:
-                cleared[prev] = 1
-                cleared[card_pos] = 1
-                guess = 0
-            elif prev != -1:
-                print deck[prev] == deck[card_pos]
-                guess += 1
-                print guess
-                if guess == 2:
-                    guess = 0
-                    exposed = [0] * 16
-        elif exposed[card_pos] == 1:
-            exposed[card_pos] = 0
-            
+            guess = 1
+            if cleared[card_pos] != 1:
+                count += 1
         prev = card_pos
     label.set_text("Turns = " + str(count))
     pass
@@ -53,17 +57,20 @@ def mouseclick(pos):
 # cards are logically 50x100 pixels in size    
 def draw(canvas):
     global CARD_WIDTH, CARD_LENGTH
-    for n in range(len(deck)): 
-        if (exposed[n] == 1 or cleared[n] == 1):
-            canvas.draw_text(str(deck[n]), ((n * CARD_WIDTH), 75), 80, "Red")
-        else:
-            point_coords = [
-                            [(n * CARD_WIDTH), 0], 
-                            [((n + 1) *CARD_WIDTH), 0], 
-                            [((n + 1)*CARD_WIDTH), CARD_LENGTH], 
-                            [(n * CARD_WIDTH), CARD_LENGTH]
-                          ]
-            canvas.draw_polygon(point_coords, 4, "Black", "Blue")
+    if (all(x == 1 for x in cleared)):
+            canvas.draw_text("Total moves: " + str(count), (130, 75), 80, "Red")
+    else:
+        for n in range(len(deck)): 
+            if (exposed[n] == 1 or cleared[n] == 1):
+                canvas.draw_text(str(deck[n]), ((n * CARD_WIDTH), 75), 80, "Red")
+            else:
+                point_coords = [
+                                [(n * CARD_WIDTH), 0], 
+                                [((n + 1) *CARD_WIDTH), 0], 
+                                [((n + 1)*CARD_WIDTH), CARD_LENGTH], 
+                                [(n * CARD_WIDTH), CARD_LENGTH]
+                              ]
+                canvas.draw_polygon(point_coords, 4, "Black", "Blue")
     pass
 
 
